b993c576ac8192b98f4423ed3dbafcc965f7c20c
famusov <famusov88@gmail.com>
2024-01-01 19:15:14 +0300

    разделил проект на слои, добавил интерфейс для для будущих репозиториев, создал DbContext, создал энтити юзера

---
+++ backend/DAL/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+public class ApplicationDbContext
+{
+    private readonly IMongoDatabase _database;
+    
+    public ApplicationDbContext(IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("MongoDBConnection");
+        var mongoClient = new MongoClient(connectionString);
+        _database = mongoClient.GetDatabase("rust-store");
+        //_userCollection = _database.GetCollection<User>("users");
+    }
+
+//    public IMongoCollection<User> _userCollection { get; set; }
+}
---
+++ backend/DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace RustStats.DAL.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+        Task DeleteRange(List<T> entityList);
+        IQueryable<T> GetAll();
+        //List<T> GetAll();
+    }
+}
