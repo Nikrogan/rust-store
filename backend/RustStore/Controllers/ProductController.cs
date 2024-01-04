aa9da96550203281add53e8dcf3235f54f2d8929
famusov <famusov88@gmail.com>
2024-01-05 02:56:17 +0300

    убрал лишнее в контроллере

--- backend/RustStore/Controllers/ProductController.cs
+++ backend/RustStore/Controllers/ProductController.cs
@@ -2,8 +2,6 @@
 using Domain.Response;
 using Microsoft.AspNetCore.Mvc;
 using Service.Interfaces;
-using System;
-using System.Text.Json;
 
 namespace RustStore.Controllers
 {
@@ -20,28 +18,14 @@ namespace RustStore.Controllers
         [HttpPost("create")]
         public async Task<IBaseServerResponse<BaseProduct>> Create(BaseProduct productModel)
         {
-            //BaseProduct? productModel = JsonSerializer.Deserialize<BaseProduct>(jsonModel);
-            //if(productModel == null)
-            //    return new BaseServerResponse<BaseProduct>(null, Domain.Enum.StatusCode.BadRequest);
-
             var response = await _productService.CreateProduct(productModel);
-
             return new BaseServerResponse<BaseProduct>(response.Data, response.StatusCode);
-        }
-
-        [HttpGet("testt")]
-        public async Task<IBaseServerResponse<BaseProduct>> Testt()
-        {
-            var a = new BaseProduct { ProductId = 12312, Description = "fdsdfgsdfahg", ImageUrl = " fsdgsadfgsag", Price = 123, Title = "fsdfgs" };
-            
-            return new BaseServerResponse<BaseProduct>(null,Domain.Enum.StatusCode.OK);
         }
 
         [HttpGet("getall")]
         public async Task<IBaseServerResponse<IEnumerable<BaseProduct>>> GetAll()
         {
             var response = await _productService.GetAllProducts();
-
             return new BaseServerResponse<IEnumerable<BaseProduct>>(response.Data, response.StatusCode);
         }
 
@@ -49,20 +33,13 @@ namespace RustStore.Controllers
         public async Task<IBaseServerResponse<BaseProduct>> Get(int id)
         {
             var response = await _productService.GetProductById(id);
-
             return new BaseServerResponse<BaseProduct>(response.Data, response.StatusCode);
-
         }
 
         [HttpPost("update")]
-        public async Task<IBaseServerResponse<BaseProduct>> Update(string jsonModel) 
+        public async Task<IBaseServerResponse<BaseProduct>> Update(BaseProduct productModel) 
         {
-            BaseProduct? productModel = JsonSerializer.Deserialize<BaseProduct>(jsonModel);
-            if (productModel == null)
-                return new BaseServerResponse<BaseProduct>(null, Domain.Enum.StatusCode.BadRequest);
-
             var response = await _productService.EditElement(productModel);
-
             return new BaseServerResponse<BaseProduct>(response.Data, response.StatusCode);
         }
 
@@ -70,7 +47,6 @@ namespace RustStore.Controllers
         public async Task<IBaseServerResponse<BaseProduct>> Delete(int id)
         {
             var response = await _productService.DeleteProductById(id);
-
             return new BaseServerResponse<BaseProduct>(null, response.StatusCode);
         }
     }
