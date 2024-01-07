ba1aaa3494ebdd31d3dfdcc368ac4eefac19a040
unknown <taifun22113@gmail.com>
2024-01-07 03:06:43 +0300

    FIX Config front

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://localhost:7208',
-    headers: {
-        'Content-Type': 'application/json', // Set the default content type for request headers
-    },
+        baseURL: 'https://localhost:7208',
+        headers: {
+            'Content-Type': 'application/json', // Set the default content type for request headers
+        }
+    }
+)
