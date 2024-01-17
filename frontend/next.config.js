b6507e349d72b190d807c6c58daabfb53aa278eb
unknown <taifun22113@gmail.com>
2024-01-17 15:16:53 +0300

    Добавил настройку Docker;

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
+      distDir: 'build',
+      output: "standalone",
+}
 
 module.exports = nextConfig
