f9cce65fd40c30f96556a2a05e6abf751bdeefe1
unknown <taifun22113@gmail.com>
2024-01-19 04:12:43 +0300

    Фикс корсов;

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,22 +9,11 @@ const nextConfig = {
       },
       distDir: 'build',
       output: "standalone",
-      async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "http://turringrust.ru" },
-                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" },
-                    { key: "Access-Control-Allow-Origin", value: "127.0.0.1:3000" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
+      experimental: {
+        serverActions: {
+          allowedOrigins: ["turringrust.ru", "localhost:3000"]
+        }
+      }
 }
 
 module.exports = nextConfig
