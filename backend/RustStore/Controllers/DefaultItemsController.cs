a178854426ea66f8d20d47a39a64e83db596f091
famus0v <famusov88@gmail.com>
2024-04-10 01:47:02 +0300

    test

--- backend/RustStore/Controllers/DefaultItemsController.cs
+++ backend/RustStore/Controllers/DefaultItemsController.cs
@@ -24,33 +24,48 @@ namespace RustStore.Controllers
         [HttpGet]
         public async Task<IBaseServerResponse<IEnumerable<DefaultItem>>> GetDefaultItems()
         {
-            //byte[] itemsData = await _cache.GetAsync("DefaultItemsCache");
+            byte[] itemsData = await _cache.GetAsync("DefaultItemsCache");
 
-            //if (itemsData != null)
-            //{
-            //    try
-            //    {
-            //        string json = Encoding.UTF8.GetString(itemsData);
-            //        IEnumerable<DefaultItem> items = JsonConvert.DeserializeObject<IEnumerable<DefaultItem>>(json);
-            //        return new BaseServerResponse<IEnumerable<DefaultItem>>(items, Domain.Enum.StatusCode.OK);
-            //    }
-            //    catch
-            //    {
-            //        return new BaseServerResponse<IEnumerable<DefaultItem>>(null, Domain.Enum.StatusCode.InternalServerError);
-            //    }
-            //}
+            try
+            {
+
+                if (itemsData != null)
+                {
+                    try
+                    {
+                        string json = Encoding.UTF8.GetString(itemsData);
+                        IEnumerable<DefaultItem> items = JsonConvert.DeserializeObject<IEnumerable<DefaultItem>>(json);
+                        return new BaseServerResponse<IEnumerable<DefaultItem>>(items, Domain.Enum.StatusCode.OK);
+                    }
+                    catch
+                    {
+                        return new BaseServerResponse<IEnumerable<DefaultItem>>(null, Domain.Enum.StatusCode.InternalServerError);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             var response = await _defaultItemService.GetDefaultItems();
 
-            //if(response.StatusCode == Domain.Enum.StatusCode.OK)
-            //{
-            //    string json = JsonConvert.SerializeObject(response.Data);
-            //    _ = Encoding.UTF8.GetBytes(json);
-            //    await _cache.SetAsync("DefaultItemsCache", itemsData, new DistributedCacheEntryOptions
-            //    {
-            //        AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(4)
-            //    });
-            //}
+            try
+            {
+                if (response.StatusCode == Domain.Enum.StatusCode.OK)
+                {
+                    string json = JsonConvert.SerializeObject(response.Data);
+                    _ = Encoding.UTF8.GetBytes(json);
+                    await _cache.SetAsync("DefaultItemsCache", itemsData, new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(4)
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             return new BaseServerResponse<IEnumerable<DefaultItem>>(response.Data, response.StatusCode);
         }
