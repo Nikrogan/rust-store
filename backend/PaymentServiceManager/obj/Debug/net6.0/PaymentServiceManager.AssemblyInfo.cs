a33af268e95c19d626d015184058f08183b658c4
famus0v <famusov88@gmail.com>
2024-05-11 00:36:46 +0300

    add scoped

--- backend/PaymentServiceManager/obj/Debug/net6.0/PaymentServiceManager.AssemblyInfo.cs
+++ backend/PaymentServiceManager/obj/Debug/net6.0/PaymentServiceManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentServiceManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bff36548b5361a91671f2ea5e3c52a090c4f150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cced4ce5aa8fc4df2573b7fe5317ba41cd427150")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentServiceManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentServiceManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
